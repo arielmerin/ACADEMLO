bbe68d25167ab4dae38e7e64d2954003e66e2fb4
arielmerin <arielmerino@ciencias.unam.mx>
2021-05-27 11:44:08 -0500

    Beginning with the project

---
+++ weather-app/src/ButtonConverter.js
@@ -0,0 +1,7 @@
+
+
+const ButtonConverter = ({handleClick}) =>{
+    return <button onClick={handleClick}> Degrees °F/ °C</button>
+}
+
+export default ButtonConverter;
---
+++ weather-app/src/WeatherContainer.js
@@ -0,0 +1,13 @@
+import {useEffect,useState} from 'react'
+import ButtonConverter from "./ButtonConverter";
+
+
+const WeatherContainer = () =>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default WeatherContainer;
