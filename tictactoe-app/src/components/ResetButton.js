7b1a79191f883e5c217b15d1a69c315485bc7b4f
arielmerin <arielmerino@ciencias.unam.mx>
2021-05-27 09:21:14 -0500

    Cleaning branch

--- tictactoe-app/src/components/ResetButton.js
+++
@@ -1,6 +0,0 @@
-
-const ResetButton = ({handleClick}) =>{
-    return <button style={{borderRadius: '16px', padding:'5px', marginTop:'5px', backgroundColor:'beige', borderColor:' #fbc361' }} onClick={handleClick}>Reiniciar partida</button>
-}
-
-export default ResetButton;
--- tictactoe-app/src/components/Turn.js
+++
@@ -1,9 +0,0 @@
-
-
-const Turn = ({nextTurn}) =>{
-    return ( <div>
-        <h3>El siguiente turno es: {nextTurn} </h3>
-    </div>)
-}
-
-export default Turn;
