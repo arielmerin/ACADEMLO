2c3c0c6116f653b36266ebc4c8dcc8e728a09568
arielmerin <arielmerino@ciencias.unam.mx>
2021-06-07 20:23:09 -0500

    CLEANING: erase all other directories

--- forms-app/src/components/PrimeContainer.js
+++
@@ -1,7 +0,0 @@
-
-const PrimesContainer = ({  })=>{
-    return <div>
-    </div>
-}
-
-export default PrimesContainer
--- poke-api/src/components/NumberItems.js
+++
@@ -1,9 +0,0 @@
-
-
-const NumberItems = ({handleChange, value}) =>{
-    return <input type="number" onChange={(e) =>{
-    handleChange(e.target.value)}
-    } value={value} />
-}
-
-export default NumberItems;
