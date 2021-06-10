655d6d41149e7fe4cbcb3a9e88e299488ad2baf5
arielmerin <arielmerino@ciencias.unam.mx>
2021-06-10 13:16:27 -0500

    Cleaning branch

--- todo-app/src/services/create.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-const baseURL = 'https://todos-go.herokuapp.com/api/todos'
-
-const create= (newTodo)=>{
-	return axios.post(`${baseURL}`, newTodo )
-}
-
-export default create;
--- todo-app/src/services/deleteTask.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const baseURL = 'https://todos-go.herokuapp.com/api/todos'
-
-const deleteTask = (id)=>{
-	return axios.delete(`${baseURL}/${id}`)
-}
-
-export default deleteTask
-
